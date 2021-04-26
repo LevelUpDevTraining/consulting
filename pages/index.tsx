c692b7b9082ec949ddc51e1467add7c514246e11
Andrew Bliss <andrewgbliss@gmail.com>
2021-04-26 14:47:19 +0900

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
+import ScreenSize from '@components/Debug/ScreenSize';
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen">
-      <h1>Next.js Tailwind CSS Starter</h1>
+    <div>
+      <ScreenSize />
+      <header>Header</header>
+      <main role="main" className="main-layout">
+        <section className="section-layout">
+          <div className="debug">
+            <h1>Next.js Tailwind CSS Starter</h1>
+            <p>
+              This document provides a guide to help with the important task of
+              choosing the correct Apple.
+            </p>
+          </div>
+        </section>
+        <section className="section-layout">
+          <div className="debug">
+            <h2>Introduction</h2>
+            <p>
+              This document provides a guide to help with the important task of
+              choosing the correct Apple.
+            </p>
+          </div>
+        </section>
+      </main>
+      <footer>Footer</footer>
     </div>
   );
 }
